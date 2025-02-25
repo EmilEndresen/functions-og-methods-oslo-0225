e10d75daa8422f2e9b61909030ca9b01f2ed1d2b
Emil Johan Endresen <emil@jobloop.no>
2025-02-25 11:00:20 +0100

    methods

--- script.js
+++ script.js
@@ -64,3 +64,56 @@ const calculatorTwo = (num1, num2, operator) => {
    }
 }
 console.log(calculatorTwo(8, 25, "/"))
+
+const displayBtn = document.getElementById("display-button")
+const output = document.getElementById("output")
+
+displayBtn.addEventListener("click", () => {
+   const userInput = document.getElementById("user-input").value
+   output.textContent = userInput
+   console.log("button clicked")
+})
+
+let message = "Hello WOrld!"
+
+console.log(message.toLowerCase())
+console.log(message.toUpperCase())
+console.log(message.includes("WOrld!"))
+console.log(message.includes("Rain"))
+
+let numbers = [1, 2, 3, 4, 5]
+
+console.log(numbers.push(6))
+console.log(numbers)
+
+console.log(numbers.pop())
+console.log(numbers)
+
+let doubled = numbers.map((num) => num * 2)
+console.log(doubled)
+
+let evenNumbers = numbers.filter((num) => num % 2 === 0)
+console.log(evenNumbers)
+
+let now = new Date()
+
+console.log(now.toDateString())
+console.log(now.getFullYear())
+console.log(now.getHours())
+
+let resultMethod = "     Hello, Javascript    "
+   .trim()
+   .toUpperCase()
+   .replace("JAVASCRIPT", "KARTLEGGING")
+
+console.log(resultMethod)
+
+//Array methods
+
+const carBrands = ["BMW", "Mazda", "Nissan", "Chrysler"]
+
+console.log(carBrands.join(" "))
+
+// Ikke bruk delete
+delete carBrands[2]
+console.log(carBrands)
